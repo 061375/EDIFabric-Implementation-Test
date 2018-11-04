d7f2ec110b30e6ea46621a372cf0c25fedf2b58b
Jeremy Heminger <contact@jeremyheminger.com>
2018-11-04 10:13:56 -0800

    Comments

--- EDI.Fabric/Libraries/Data/SimpleDB.cs
+++ EDI.Fabric/Libraries/Data/SimpleDB.cs
@@ -9,6 +9,14 @@ using System.Threading.Tasks;
 
 namespace EDI.Fabric
 {
+    /**
+     * I don't expect more than a few 100 records per run
+     * A simple query and loop to sort the data should suffice
+     *
+     * data[0] = {{key, value},{key, value}, etc..},
+     * data[1] = {{key, value},{key, value}, etc..}, 
+     * etc...
+     * */
     class SimpleDB
     {
         private static SqlConnection sqlConnection1 = null;
@@ -45,7 +53,8 @@ namespace EDI.Fabric
                     for (int i = 0; i < count; i++)
                     {
 
-                        Console.WriteLine(row + " " + reader.GetName(i) + " " + reader.GetValue(i));
+                        // Console.WriteLine(row + " " + reader.GetName(i) + " " + reader.GetValue(i)); // debug
+                        
                         if(false == col.ContainsKey(reader.GetName(i).ToString()))
                             col.Add(reader.GetName(i).ToString(), reader.GetValue(i).ToString());
                         /*
