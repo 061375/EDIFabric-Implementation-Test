41da2134b413df1d4d959f350b67716ed5ebe4b8
jeremy <j.heminger@gmail.com>
2018-11-12 08:04:32 -0800

    Add constants file

---
+++ EDI.Fabric/Constants.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDI.Fabric
+{
+    public static class Constants
+    {
+        // ---- X12856
+
+        // HL
+        public const string X12856HL1 = "1";
+        public const string X12856HL3 = "S";
+
+        //TD1
+        public const string X12856TD101 = "";
+        public const string X12856TD102 = "";
+        public const string X12856TD106 = "";
+        public const string X12856TD107 = "";
+        public const string X12856TD108 = "LB";
+        public const string X12856TD109 = "";
+        public const string X12856TD110 = "";
+
+        // TD5
+        public const string X12856TD501 = "";
+        public const string X12856TD502 = "";
+        public const string X12856TD503 = "JBHT";
+        public const string X12856TD504 = "";
+
+        // TD3
+        public const string X12856TD301 = "";
+        public const string X12856TD302 = "ABCDEF";
+        public const string X12856TD303 = "07213567";
+        public const string X12856TD309 = "30394938483234";
+
+        // REF
+        public const string X12856REF01 = "";
+        public const string X12856REF02 = "";
+
+        // DTM
+        public const string X12856DTM01 = "011";
+        public const string X12856DTM02 = "200";
+
+        // N1
+        public const string X12856N101 = "ST";
+
+        // N2
+        public const string X12856N201 = "SF";
+
+        // ----- X12850
+
+        // N1
+        public const string X12850N101 = "SF";
+
+        // PO1
+        public const string X12850PO101 = "";
+
+        // BEG
+        public const string X12856BEG01 = "";
+        public const string X12856BEG02 = "";
+    }
+}
